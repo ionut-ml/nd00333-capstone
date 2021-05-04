8cb7d6f0e3612595fe3db578e3c572c1b861ce47
ionut-ml <66036303+ionut-ml@users.noreply.github.com>
2021-05-04 08:40:07 +0000

    Added train.py file for HDR Run

---
+++ starter_file/train.py
@@ -0,0 +1,59 @@
+from sklearn.ensemble import RandomForestClassifier
+import argparse
+import os
+import numpy as np
+from sklearn.metrics import mean_squared_error
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+from azureml.core.run import Run
+from azureml.data.dataset_factory import TabularDatasetFactory
+
+# TODO: Create TabularDataset using TabularDatasetFactory
+# Data is located at:
+# "https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data"
+
+ds = TabularDatasetFactory.from_delimited_files("./heart.csv")
+
+def clean_data(data):
+    
+    x_df = data.to_pandas_dataframe().dropna()
+    
+    y_df = x_df.pop("target")
+
+    return x_df, y_df
+
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+
+run = Run.get_context()
+
+def main():
+    # Add arguments to script
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--n_estimators', type=int, default=100, help="The number of trees in the forest.")
+    parser.add_argument('--max_depth', type=int, default=None, help="The maximum depth of the tree. If None, then nodes are expanded until all leaves are pure or until all leaves contain less than min_samples_split samples.")
+    parser.add_argument('--min_samples_leaf', type=int, default=1, help="The minimum number of samples required to be at a leaf node. A split point at any depth will only be considered if it leaves at least min_samples_leaf training samples in each of the left and right branches. This may have the effect of smoothing the model, especially in regression.")
+
+
+    args = parser.parse_args()
+
+    run.log("Number of trees:", np.float(args.n_estimators))
+    run.log("Max depth:", np.int(args.max_depth))
+    run.log("Min number of samples at leaf node:", np.int(args.min_samples_leaf))
+
+    model = RandomForestClassifier(n_estimators=args.n_estimators, max_depth=args.max_depth, min_samples_leaf=args.min_samples_leaf, random_state=42).fit(x_train, y_train)
+
+    accuracy = model.score(x_test, y_test)
+    run.log("Accuracy", np.float(accuracy))
+
+    os.makedirs('./outputs', exist_ok=True)
+    joblib.dump(value=model, filename='./outputs/model.joblib')
+
+if __name__ == '__main__':
+    main()
