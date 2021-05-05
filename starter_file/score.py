71c0f3c9bb1c6bbfd9f8ba39d317d5bab3ec772d
ionut-ml <66036303+ionut-ml@users.noreply.github.com>
2021-05-05 09:33:35 +0000

    Failing deployment

--- starter_file/score.py
+++ starter_file/score.py
@@ -1,9 +1,27 @@
 import json
+import os
+from sklearn.externals import joblib
+import pandas as pd
+from azureml.core.model import Model
+import pickle
 
 def init():
-    print('This is init')
+    global model
+    model_path = os.path.join(os.getnev('AZUREML_MODEL_DIR'), 'model.joblib')
+    # Check if model is in location
+    print("Found model: ", os.path.isfile(model_path))
+    model = joblib.load(model_path)
 
 def run(data):
-    test = json.loads(data)
-    print(f'received data {test}')
-    return(f'test is {test}')
+    try:
+        # Read in json data
+        test_data = json.loads(data)['data']
+        # Convert data to a Pandas DataFrame
+        data_df = pd.DataFrame(test_data)
+        # Make a prediction using loaded model
+        result = model.predict(data_df)
+        # Return result as json
+        return json.dumps({"result": result.tolist()})
+    except Exception as e:
+        error = str(e)
+        return error
